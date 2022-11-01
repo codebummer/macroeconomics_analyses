1dd770547853a4f60066411d7b3d903ed234d9e7
codebummer <holysmokes@hanmail.net>
2022-11-01 22:07:00 +0900

    Update pdf_processor_without_errors.py

--- pdf_processor_without_errors.py
+++ pdf_processor_without_errors.py
@@ -1,4 +1,3 @@
-
 import os
 import PyPDF2
 import math
@@ -16,6 +15,11 @@ class locator:
         path = 'D:/myprojects/macroeconomics_analyses/'
         self.pdf_object = open(path+filename, 'rb')
         self.pdf_read = PyPDF2.PdfFileReader(self.pdf_object)
+        self.ISEXPORT = True
+        if find_word != '월별 수출실적':
+            self.ISEXPORT = False
+               
+        print(self.ISEXPORT)
         
         for page in range(self.pdf_read.numPages):
             page_object = self.pdf_read.getPage(page)
@@ -31,29 +35,33 @@ class locator:
         start = text.find(find_start)
         end = text.find(find_end)
         
-        excerpt = text[start+2:end-2].replace('\n', ' ').strip().split(' ')                  
+        if self.ISEXPORT is True:
+            excerpt = text[start+2:end-2].replace('\n', ' ').strip().split(' ')                  
+        else:
+            excerpt = text[start:end-2].replace('\n', '').strip().split(' ')
 
         brokendown = []
         added = []
         for element in excerpt:
-            asc = ord(element[0])
-            if 12593 <= asc and asc <= 52044:                
-                brokendown.append(added)
-                added = [element]
+            asc = ord(element[0]) 
+            if 12593 <= asc and asc <= 52044:
+                if self.ISEXPORT is True:                
+                    brokendown.append(added)
+                    added = [element]
+                else:
+                    
             else:
                 added.append(element) 
         
         extracted = pd.DataFrame([])
         for num, element in enumerate(brokendown):
-            if num is 0:
-                extracted['Month'] = element
-            # col_name = [element[0].strip()]
-            # col = element[1:]
+            if num is 0 and self.ISEXPORT is True:
+                extracted['월'] = element
+
             else:
-                extracted[element[0]] = element[1:]
-            
+                extracted[element[0]] = element[1:]            
                         
-        # print(extracted)
         return extracted
 
-sep22 = locator('2022년 9월 수출입동향.pdf', '월별 수출실적', '%)', '수출액 및')
+sep22_export = locator('2022년 9월 수출입동향.pdf', '월별 수출실적', '%)', '수출액 및')
+sep22_import = locator('2022년 9월 수출입동향.pdf', '월별 수입실적', '%)', '연간 수입액 및')
